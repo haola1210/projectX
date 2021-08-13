a3c52d23855f2053a8200e507c14a7f697cbb076
unknown <longbestlee1909@gmail.com>
2021-08-13 17:01:57 +0700

    template color and install styled components

---
+++ client/src/styles/GlobalStyle.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components"
+
+export default GlobalStyle = createGlobalStyle`
+    *{
+        padding : 0;
+        margin : 0;
+        outline : none;
+        box-sizing : border-box;
+        font-family : sans-serif;
+    }
+`
---
+++ client/src/styles/Themes.jsx
@@ -0,0 +1,21 @@
+const main = {
+    secondaryText : "#777777",
+    hover : "#CAD2C5",
+    active : "#CAD2C5",
+    link : "#FFE626",
+    receivedMessBg : "#CAD2C5",
+    sentMessBg : "#FFA200",
+
+}
+
+export const lightTheme = {
+    ...main,
+    bodyBg : "#EBEBEB",
+    text : "black",
+}
+
+export const darkTheme = {
+    ...main,
+    bodyBg : "black",
+    text : "#EBEBEB"
+}
