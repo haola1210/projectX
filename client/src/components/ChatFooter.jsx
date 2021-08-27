1b9c2e8bac41fc441ee5cd2ec79588018bd37583
unknown <longbestlee1909@gmail.com>
2021-08-27 22:42:41 +0700

    upload image and preview upload img ok, notice: after upload should set null for e.target.value

--- client/src/components/ChatFooter.jsx
+++ client/src/components/ChatFooter.jsx
@@ -4,32 +4,74 @@ import {
     FiSmile
 } from "react-icons/fi";
 import { IoSend } from "react-icons/io5";
+import { TiDeleteOutline } from "react-icons/ti";
+
+import useUploadImg from '../hooks/useUploadImg';
 import CustomTextArea from './CustomTextArea';
 
+
+const PreviewImg = ({ className, src, remove }) => (
+    <div className="relative p-2" >
+        <span 
+            className="absolute top-0 right-0 rounded-full bg-red-500 text-gray-800" 
+            onClick={remove}
+        ><TiDeleteOutline /></span>
+        <img src={src}
+            className="h-12 w-auto rounded-md" 
+            // style={{ backgroundImage : `url(${src})` }} 
+        />
+    </div>
+) 
 
 function ChatFooter(props) {
     
     const [text, setText] = useState("")
+    
+    const [data, onChange, onDone, onRemoveOne ] = useUploadImg()
 
     return (
-        <div className="w-full flex flex-row items-center p-2">
+        <>  
+            {/* preview upload images */}
+            <div className="flex flex-row justify-center items-center">
+                {
+                    data.previews.map((url, index) => 
+                        <PreviewImg 
+                            key={index}
+                            src={url} 
+                            remove={() => onRemoveOne(index)}
+                        />
+                    )
+                }
+            </div>
 
-            <CustomTextArea 
-                setText={setText}
-                className="flex-grow p-2 text-sm mr-2" 
-                placeholder="Nhắn gì đó đi..."
-            />
-
-            <span className="rounded-btn">
-                <FiImage />
-            </span>
-            <span className="rounded-btn ml-1">
-                <FiSmile />
-            </span>
-            <span className="rounded-btn ml-1" onClick={() => console.log(text)}>
-                <IoSend />
-            </span>
-        </div>
+            {/* main */}
+            <div className="w-full flex flex-row items-center p-2">
+                <CustomTextArea 
+                    setText={setText}
+                    className="flex-grow p-2 text-sm mr-2" 
+                    placeholder="Nhắn gì đó đi..."
+                />
+                <label htmlFor="upload">
+                    <span className="rounded-btn">
+                        <input 
+                            type="file" 
+                            id="upload" 
+                            className="hidden" 
+                            multiple={true} 
+                            onChange={onChange}
+                        />
+                        
+                        <FiImage />
+                    </span>
+                </label>
+                <span className="rounded-btn ml-1">
+                    <FiSmile />
+                </span>
+                <span className="rounded-btn ml-1" onClick={() => console.log(text)}>
+                    <IoSend />
+                </span>
+            </div>
+        </>
     );
 }
 
---
+++ client/src/hooks/useUploadImg.js
@@ -0,0 +1,80 @@
+import { useEffect, useReducer } from "react"
+
+const initialState = {
+    files : [],
+    previews : []
+}
+
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case "UPLOAD":
+            return {
+                ...state,
+                files : [...state.files, ...action.payload.files],
+                previews : [...state.previews, ...action.payload.previews]
+            }
+
+        case "DELETE_ONE":
+            return {
+                ...state,
+                files : [...action.payload.files],
+                previews : [...action.payload.previews]
+            }
+
+        default:
+            return {
+                ...initialState
+            }
+        
+    }
+}
+
+const useUploadImg = () => {
+    const [data, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        console.log(data)
+    })
+
+    const onChange = (e) => {
+        console.log("uploaded")
+        console.log(e.target.files)
+        const files = [...e.target.files]
+        const dataURLs = files.map(file => URL.createObjectURL(file))
+
+        dispatch({
+            type: "UPLOAD",
+            payload : {
+                files,
+                previews : dataURLs
+            }
+        })
+
+        e.target.value = null
+    }
+
+    const onDone = () => {
+        dispatch({
+            type : "DELETE_ALL"
+        })
+    }
+
+    const onRemoveOne = (index) => {
+        const newFiles = data.files.filter((_, id) => id !== index)
+        const newPreviews = data.previews.filter((_, id) => id !== index)
+
+        dispatch({
+            type : "DELETE_ONE",
+            payload : {
+                files : newFiles,
+                previews : newPreviews
+            }
+        })
+    }
+
+    return [data, onChange, onDone, onRemoveOne]
+
+}
+
+export default useUploadImg
